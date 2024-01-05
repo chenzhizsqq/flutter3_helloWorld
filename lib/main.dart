bd70ebd71398c6f1ad0036bcb2cea220b56964c3
orange2 <chenzhi@qq.com>
2024-01-05 05:28:07 +0000

    test

--- lib/main.dart
+++ lib/main.dart
@@ -23,18 +23,25 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        //背景色
+        backgroundColor: Colors.orange,
         // The title text which will be shown on the action bar
         title: Text(title),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('Row 1'),
+            Text('Row 2'),
+            Text('Row 3'),
+          ],
         ),
       ),
     );

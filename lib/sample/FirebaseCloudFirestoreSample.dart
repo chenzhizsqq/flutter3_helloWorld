56a91953abec2aebd0cc327410b30464b5633d95
orange2 <chenzhi@qq.com>
2024-01-15 09:36:07 +0000

    Firebase CloudFirestore Sample:70%

--- lib/sample/FirebaseCloudFirestoreSample.dart
+++ lib/sample/FirebaseCloudFirestoreSample.dart
@@ -12,12 +12,16 @@ class _FirebaseCloudFirestoreSampleState
     extends State<FirebaseCloudFirestoreSample> {
   List<DocumentSnapshot> documentList = []; // 作成したドキュメント一覧
 
+  // 指定したドキュメントの情報
+  String orderDocumentInfo = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           children: <Widget>[
+            //11111
             ElevatedButton(
               child: Text('コレクション＋ドキュメント作成'),
               onPressed: () async {
@@ -28,6 +32,8 @@ class _FirebaseCloudFirestoreSampleState
                     .set({'name': '鈴木', 'age': 40}); // データ
               },
             ),
+
+            //2222
             ElevatedButton(
               child: Text('サブコレクション＋ドキュメント作成'),
               onPressed: () async {
@@ -38,6 +44,53 @@ class _FirebaseCloudFirestoreSampleState
                     .collection('orders') // サブコレクションID
                     .doc('id_123') // ドキュメントID << サブコレクション内のドキュメント
                     .set({'price': 600, 'date': '9/13'}); // データ
+              },
+            ),
+
+            //4444
+            ElevatedButton(
+              child: Text('ドキュメントを指定して取得'),
+              onPressed: () async {
+                // コレクションIDとドキュメントIDを指定して取得
+                final document = await FirebaseFirestore.instance
+                    .collection('users')
+                    .doc('id_abc')
+                    .collection('orders')
+                    .doc('id_123')
+                    .get();
+                // 取得したドキュメントの情報をUIに反映
+                setState(() {
+                  orderDocumentInfo =
+                      '${document['date']} ${document['price']}円';
+                });
+              },
+            ),
+            // ドキュメントの情報を表示
+            ListTile(title: Text(orderDocumentInfo)),
+
+            //5555
+            ElevatedButton(
+              child: Text('ドキュメント更新'),
+              onPressed: () async {
+                // ドキュメント更新
+                await FirebaseFirestore.instance
+                    .collection('users')
+                    .doc('id_abc')
+                    .update({'age': 41});
+              },
+            ),
+
+            //6666
+            ElevatedButton(
+              child: Text('ドキュメント削除'),
+              onPressed: () async {
+                // ドキュメント削除
+                await FirebaseFirestore.instance
+                    .collection('users')
+                    .doc('id_abc')
+                    .collection('orders')
+                    .doc('id_123')
+                    .delete();
               },
             ),
           ],

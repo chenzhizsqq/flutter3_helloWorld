3f3107f5339db40ada153c6887d4fb083a2fa77f
orange2 <chenzhi@qq.com>
2024-01-18 05:53:16 +0000

    FirebaseRealtimeDatabaseSample initState ready

--- lib/sample/FirebaseRealtimeDatabaseSample.dart
+++ lib/sample/FirebaseRealtimeDatabaseSample.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class FirebaseRealtimeDatabaseSample extends StatefulWidget {
@@ -11,8 +12,35 @@ class FirebaseRealtimeDatabaseSample extends StatefulWidget {
 
 class _FirebaseRealtimeDatabaseSampleState
     extends State<FirebaseRealtimeDatabaseSample> {
+  bool initialized = false;
+  late DatabaseReference _counterRef;
+  late DatabaseReference _messagesRef;
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  Future<void> init() async {
+    _counterRef = FirebaseDatabase.instance.ref('counter');
+
+    final database = FirebaseDatabase.instance;
+
+    _messagesRef = database.ref('messages');
+
+    database.setLoggingEnabled(false);
+
+    setState(() {
+      initialized = true;
+    });
+    print("init() end");
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (!initialized) return Container();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orange,

0748d292604b37b383932115adfed7ec4d6fa45c
orange2 <chenzhi@qq.com>
2024-01-18 04:32:05 +0000

    firebase_database import

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ import 'sample/FirebaseFirestoreStream.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 //void main() => runApp(const MyApp());
 

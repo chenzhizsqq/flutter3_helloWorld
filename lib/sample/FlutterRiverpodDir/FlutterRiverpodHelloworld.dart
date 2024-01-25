e0deaf2ad18d708c395075ba91b81a26f4e30139
orange2 <chenzhi@qq.com>
2024-01-25 07:02:44 +0000

    FlutterRiverpod  Helloworld ok

--- lib/sample/FlutterRiverpodDir/FlutterRiverpodHelloworld.dart
+++ lib/sample/FlutterRiverpodDir/FlutterRiverpodHelloworld.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+//StateNotifierProvider 是用来监听和暴露 StateNotifier （在 state_notifier 包中，Riverpod 又将其重新导出） 的 provider 。
 final counterProvider = StateNotifierProvider<Counter, int>((ref) {
   return Counter();
 });
 
+//StateProvider…変数 StateNotifier 的优势：本质上是不可变的。比较新旧状态并自动通知侦听器。
 class Counter extends StateNotifier<int> {
   Counter() : super(0);
   void increment() => state++;
 }
 
+//StatelessWidget = ConsumerWidget
+//StatefulWidget + State = ConsumerStatefulWidget + ConsumerState
 class FlutterRiverpodHelloworld extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {

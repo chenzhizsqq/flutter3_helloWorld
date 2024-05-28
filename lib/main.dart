54e00ad107cc5a6bafd933e914c57979b631475a
orange2 <chenzhi@qq.com>
2024-05-28 06:42:57 +0000

    test git

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 
 //void main() => runApp(const MyApp());
+//
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
